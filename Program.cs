3dc81612458f77365281a82c7ac07d2f54c7bc0b
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-05-27 22:56:32 +0530

    [Harsh]add: Added a new class fibonacci

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibonacciLength;
+            int f1 = 0, f2 = 1, f3;
+
+            Console.WriteLine("Enter how many fibonacci numbers you want : ");
+            fibonacciLength = int.Parse(Console.ReadLine());
+            Console.Write(f1 + " " + f2 + " ");
+
+            for (int i = 2; i < fibonacciLength; ++i)
+            {
+                f3 = f1 + f2;
+                Console.Write(f3 + " ");
+                f1 = f2;
+                f2 = f3;
+
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
