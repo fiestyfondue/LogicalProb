18a275d53491f9db58e201defa05aaeea703316f
fiestyfondue <84001951+fiestyfondue@users.noreply.github.com>
2021-05-28 17:55:39 +0530

    [Harsh]add:- new class added Revrse

---
+++ reverse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fibonacci
+{
+    class reverse
+    {
+
+       static void Main(string[] args)
+       {     
+            int num = 1234;
+            int result = 0;
+            int rem;
+
+            while (num != 0)
+            {
+            rem = num % 10;
+            result = result * 10 + num;
+            num = num / 10;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+                
+       }
+    }
+}
